a4bc5b5664e66d60be851e6f0066bf02d9e7bf6b
hakunekooo <hakunekooo@proton.me>
2023-06-18 03:08:41 +0600

    add is_palindrome

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+def is_palindrome(word):
+    # Convert the word to lowercase and remove spaces
+    word = word.lower().replace(" ", "")
+    
+    # Check if the word is a palindrome
+    if word == word[::-1]:
+        return True
+    else:
+        return False
+
+# Test the function
+word = input("Enter a word: ")
+if is_palindrome(word):
+    print("It's a palindrome!")
+else:
+    print("It's not a palindrome.")
