7d8f3eeae785cfe2040493f8a26a4e02e635a2c3
hakunekooo <hakunekooo@proton.me>
2023-06-18 03:13:46 +0600

    add roll_dice function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 def is_palindrome(word):
     # Convert the word to lowercase and remove spaces
     word = word.lower().replace(" ", "")
@@ -6,11 +7,19 @@ def is_palindrome(word):
     if word == word[::-1]:
         return True
     else:
-        return False
+        return Falseg
 
-# Test the function
-word = input("Enter a word: ")
-if is_palindrome(word):
-    print("It's a palindrome!")
-else:
-    print("It's not a palindrome.")
+def roll_dice():
+    # Simulate rolling two dice
+    dice_1 = random.randint(1, 6)
+    dice_2 = random.randint(1, 6)
+    
+    # Calculate the total
+    total = dice_1 + dice_2
+    
+    # Print the result
+    print(f"The dice rolled: {dice_1}, {dice_2}")
+    print(f"Total: {total}")
+
+# Roll the dice
+roll_dice()
